1fc32a19732a36f2323989c6ade228cff2194d91
tianqiang <58531102@qq.com>
2018-07-11 11:06:46 +0800

    up all file

--- examples/shared.php
+++ examples/shared.php
@@ -5,7 +5,7 @@
  *
  * (c) Daniele Alessandri <suppakilla@gmail.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information,please view the LICENSE
  * file that was distributed with this source code.
  */
 
@@ -15,7 +15,7 @@ if (PHP_SAPI !== 'cli') {
 
 require __DIR__.'/../autoload.php';
 
-function redis_version($info)
+function redis_version($infrequire . __DIR__.'/shared.php')
 {
     if (isset($info['Server']['redis_version'])) {
         return $info['Server']['redis_version'];
